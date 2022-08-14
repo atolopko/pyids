09af39c733c627ff7a1a9c28c5b70baa98ac02f4
Andrew Tolopko <atolopko@chanzuckerberg.com>
2022-08-13 21:00:43 -0600

    collect identifiers recursively

--- identifiers.py
+++ identifiers.py
@@ -1,35 +1,58 @@
-from os import PathLike
 import sys
+from collections import defaultdict
+from os import PathLike
+from pprint import pp
+from symtable import symtable, SymbolTable
+from typing import List, Dict
 
-from symtable import symtable, SymbolTable, Function
-from typing import Tuple
+Identifiers = Dict[str, List[str]]
 
 
-def parse_identifiers(source_file: PathLike):
+def parse_identifiers(source_file: PathLike) -> Identifiers:
     with open(source_file) as f:
         st = symtable(f.read(), str(source_file), 'exec')
-    walk(st)
+    return collect(st)
 
 
-def print_attributes(st: SymbolTable, level, *attrs):
-    indent = "  " * (level + 1)
-    for a in attrs:
-        val = eval(f"st.get_{a}()")
-        if isinstance(val, Tuple):
-            val = ' '.join(val)
-        print(f"{indent}{a}: {val}")
+def collect(st: SymbolTable, namespace: str = '') -> Identifiers:
+    identifiers = defaultdict(list)
 
+    sym_name = namespace + st.get_name()
+    if st.get_type() == 'function':
+        identifiers['fns'].append(sym_name)
+    if st.get_type() == 'class':
+        identifiers['classes'].append(sym_name)
+    if st.get_type() == 'module':
+        identifiers['modules'].append(sym_name)
 
-def walk(st: SymbolTable, level: int = 0) -> None:
-    indent = "  " * level
-    print(f"{indent}- {st.get_name()}")
-    print_attributes(st, level, "type")
-    if isinstance(st, Function):
-        f = st
-        print_attributes(st, level, 'parameters', 'locals', 'nonlocals')
+    namespace += f"{st.get_name()}."
+
+    for s in st.get_symbols():
+        if s.is_namespace():
+            continue
+
+        sym_name = namespace + s.get_name()
+
+        if s.is_parameter():
+            identifiers['fn_params'].append(sym_name)
+        elif s.is_local():
+            if st.get_type() == 'function':
+                identifiers['fn_locals'].append(sym_name)
+            elif st.get_type() == 'class':
+                identifiers['cls_locals'].append(sym_name)
+            elif st.get_type() == 'module':
+                identifiers['mod_locals'].append(sym_name)
+        elif s.is_global():
+            identifiers['globals'].append(sym_name)
+
+    # recurse
     for c in st.get_children():
-        walk(c, level + 1)
+        nested_identifiers = collect(c, namespace)
+        for k in nested_identifiers.keys():
+            identifiers[k].extend(nested_identifiers[k])
+
+    return identifiers
 
 
 if __name__ == '__main__':
-    parse_identifiers(sys.argv[1])
+    pp(parse_identifiers(sys.argv[1]))
